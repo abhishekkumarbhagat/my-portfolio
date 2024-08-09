ce568bd65f1a529a7fef8f308314220f9dc23456
abhishekkumarbhagat <abhishekbhagat021@gmail.com>
2024-08-09 10:22:15 +0530

    Initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,418 @@
+import companylogo from '../images/314e.jpg'
+import juspaylogo from '../images/juspay.jpg'
+import iitropar from '../images/iitropar.png'
+import cmrit from '../images/cmr.png'
+import bseb from '../images/bseb.jpg'
+import hdpui from '../images/hdpui.jpg'
+
+import fastapi from '../images/fastapi.jpg'
+import generativeai from '../images/generativeai.png'
+import llm from '../images/llm.png'
+import numpy from '../images/numpy.png'
+import pandas from '../images/pandas.png'
+import tensorflow from '../images/tensorflow.png'
+import matplotlib from '../images/matplotlib.png'
+import nlp from '../images/nlp.png'
+import nltk from '../images/nltk.png'
+import vectordb from '../images/vespa.jpg'
+import cv from '../images/cv.png'
+import deeplearning from '../images/deeplearning.png'
+import math from '../images/math.jpg'
+import git from '../images/github.png'
+import pycharm from '../images/pycharm.jpg'
+import dsa from '../images/datastructure.png'
+import openai from '../images/openai.png'
+import ollama from '../images/ollama.png'
+import prompteng from '../images/promt.png'
+import nosql from '../images/nosql.png'
+import ai from '../images/aiml.png'
+import phr from '../images/phr.png'
+import penknife from '../images/penknife.png'
+import dexit from '../images/dexit.png'
+import jeeves from '../images/jeeves.png'
+
+
+export const Bio = {
+  name: "Abhishek Kumar",
+  roles: [
+    "Software Developer",
+    "AI/ML Engineer",
+    "FastAPI Developer",
+    "Gen AI Engineer",
+  ],
+  description:
+    "At 314e Corporation, our team leverages my expertise in AI and Generative AI to architect innovative full-stack solutions. My educational foundation from CMR Institute Of Technology, blended with practical experience in Large Language Model Operations and robust analytical skills, empowers us to meet complex requirements with precision. Transitioning from JUSPAY as a Technical Product Analyst, I've cultivated a deep understanding of product analytics and version control, which complements my recent certification in Supervised Machine Learning. This multidisciplinary approach ensures that we deliver cutting-edge solutions, keeping us ahead in the fast-evolving tech landscape.",
+  github: "https://github.com/ironman8132",
+  resume:
+    "https://drive.google.com/file/d/1dq2sjUm0mUmcvoAzYR4-uOva44t3WhLU/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/abhishek-kumar-bhagat/",
+  twitter: "",
+  insta: "https://www.instagram.com/_the_discipline_king/",
+  facebook: "https://www.facebook.com/profile.php?id=100027361351388",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "FastAPI",
+        image: fastapi,
+      },
+      {
+        name: "Redis",
+        image: "https://www.vectorlogo.zone/logos/redis/redis-ar21.svg",
+      },
+      {
+        name: "Pytest",
+        image: "https://www.vectorlogo.zone/logos/pytest/pytest-ar21.svg",
+      },
+      {
+        name: "Jinja2",
+        image: "https://www.vectorlogo.zone/logos/pocoo_jinja/pocoo_jinja-ar21.svg",
+      },
+      {
+        name: "SQL Alchemy",
+        image: "https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "AI and ML",
+    skills: [
+      {
+        name: "Generative AI",
+        image: generativeai,
+      },
+      {
+        name: "RAG",
+        image:ai,
+      },
+      {
+        name: "LLM",
+        image:llm,
+      },
+      {
+        name: "Modal Building/Fine Tuning",
+        image:llm,
+      },
+      {
+        name: "Numpy",
+        image: numpy,
+      },
+      {
+        name: "Pandas",
+        image:pandas,
+      },
+      {
+        name: "TensorFlow",
+        image: tensorflow,
+      },
+      {
+        name: "MatPlotLib",
+        image:matplotlib,
+      },
+      {
+        name: "CNN",
+        image:cv,
+      },
+      {
+        name: "NLP",
+        image:nlp,
+      },
+      {
+        name: "NLTK",
+        image:nltk,
+      },
+      {
+        name: "Vector Database",
+        image:vectordb,
+      },
+      {
+        name: "OpenCV",
+        image:cv,
+      },
+      {
+        name: "Deep Learning",
+        image:deeplearning,
+      },
+      {
+        name: "Mathmatics",
+        image:math,
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          git,
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "PyCharm",
+        image:pycharm,
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "DSA",
+        image:dsa,
+      },
+      {
+        name: "OpenAI",
+        image:openai,
+      },
+      {
+        name: "Ollama",
+        image:ollama,
+      },
+      {
+        name: "Prompt Engineering",
+        image:prompteng,
+      },
+      {
+        name: "NoSQL",
+        image: nosql
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: companylogo,
+    role: "Software Developer",
+    company: "314e Corporation Private Limited",
+    date: "1st August 2022 - Present",
+    desc: "As a key contributor at 314e Corporation, I focus on developing and implementing innovative full-stack solutions, particularly for specialized healthcare software. My work includes backend development, data security, and creating user-friendly interfaces, leveraging my skills in AI and Generative AI.",
+    skills: [
+      "Python",
+      "Docker",
+      "FastAPI",
+      "AI/ML",
+      "SQL",
+      "PostgreSQL",
+      "NLP",
+      "DeepLearning",
+      "OpenCV",
+      "RAG",
+      "TensorFlow",
+      "Generative AI",
+      "React.js"
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 0,
+    img: juspaylogo,
+    role: "Technical Product Analyst Intern",
+    company: "Juspay",
+    date: "April 2022 - Auust 20",
+    desc: "At Juspay Private Limited, I served as a Technical Product Analyst, focusing on testing and troubleshooting to enable seamless data sharing among sub-merchants and leading the development of innovative solutions.",
+    skills: [
+      "Postman",
+      "Pytest",
+      "Integration Testing",
+      "Unit Testing",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://drive.google.com/file/d/17Hjcbe1FT-sLGSWGea7Lwhq_-Qr6kiYu/view?usp=sharing",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: iitropar,
+    school: "Indian Institute of Technology, Ropar",
+    date: "August 2024 - Present",
+    grade: "-",
+    desc: "I am currently pursuing a minor degree in AI and machine learning at the Indian Institute of  Technology, Ropar",
+    degree: "Minor, Artificial Intelligence",
+  },
+  {
+    id: 1,
+    img: cmrit,
+    school: "CMR Institute of Technology, Bangalore",
+    date: "July 2018 - August 2022",
+    grade: "8.01 CGPA",
+    desc: "I have completed my Bachelor's degree in Computer Science and Engineering from CMR Institute of Technology, Bangalore, where I gained strong foundational knowledge in various computer science disciplines like AI/ML, data structures, web development, etc.",
+    degree: "Bachelor of Engineering - BE, Computer Science and Engineering",
+  },
+  {
+    id: 2,
+    img: bseb,
+    school: "Rajkiya High School, Jamui",
+    date: "Apr 2015 - Apr 2017",
+    grade: "62.8%",
+    desc: "I completed my class 12 high school education at Rajkiya High School, Jamui.",
+    degree: "Intermidiate(XII), Science with Mathmatics",
+  },
+  {
+    id: 3,
+    img: bseb,
+    school: "Rani Prabhavati High School, Sangrampur(Munger)",
+    date: "Apr 2014 - Apr 2015",
+    grade: "75.0%",
+    desc: "I completed my class 10 education at Rani Prabhavati High School, Sangrampur(Munger).",
+    degree: "10th, All Subjects",
+  },
+];
+
+export const projects = [
+  {
+    id: 3,
+    title: "Health Data Platform",
+    date: "october 2022 - December 2022",
+    description:
+    'The Health Data Platform (HDP) serves as a specialized data visualization platform tailored for healthcare organizations within the United States. Spearheaded the development of a dynamic web application UI for the "Health Data Platform" project, encompassing pivotal modules: financial dashboard, clinical dashboard, and user management system. Established seamless data flow between the front-end and back-end by implementing Axios for efficient data retrieval from the back-end. Designed and integrated a comprehensive range of data visualization tools, including line charts, bar charts, treemaps, and matrices, to render intricate data sets in an insightful graphical format. Designed and executed sophisticated SQL queries to extract and analyze patient data, ensuring accurate retrieval and comprehensive insights. Orchestrated the creation of back-end endpoints using FastAPI to seamlessly transmit data generated by the Data Team, ensuring accurate and timely data integration. Seamlessly integrated Keycloak functionality by creating essential back-end endpoints to facilitate role creation, user management (creation, update, deletion), and role assignment, thereby enhancing user authentication and authorization.',
+    image: hdpui,
+    tags: [
+      "JWT",
+      "eCharts",
+      "Antd",
+      "Eslint",
+      "JavaScript",
+      "Typescript",
+      "React Js",
+      "Axios",
+      "Keycloak",
+      "CSS",
+      "Redux",
+    ],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 4,
+    title: "PHR",
+    date: "Apr 2023 - May 2023",
+    description:
+      'Patient Health Record is a project, that enables US citizens to access and review their health records and insurance details seamlessly. Developed a comprehensive web application user interface for the "Patient Health Record" project, enabling USA citizens to access and review their health records and insurance details seamlessly. Orchestrated the integration of OAuth2 protocols to securely collect patient data from various third-party sources, demonstrating expertise in OAuth2 setup and testing for enhanced data acquisition.',
+    image: phr,
+    tags: ["React Js", "FastApi", "OAuth2", "Pydantic", "Casbin"],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 0,
+    title: "Dexit - AI powered Documentation System",
+    date: "Oct 2022 - Present",
+    description:'I developed backend APIs using FastAPI to interact with a diverse document database that includes handwritten, text-typed PDFs, and PDFs created from images. For data processing, I utilized PyMuPDF and Tesseract OCR to extract text from these various PDF formats, which facilitated effective document retrieval. Additionally, I fine-tuned the "llma3" model using Transformers and PyTorch, improving search performance from 79% to 88%.',
+    image:dexit,
+    tags: [
+      "OCR",
+      "Tesseract",
+      "CNN",
+      "TensorFlow",
+      "llma3",
+      "ollama",
+      "PyMuPDF",
+      "LLM",
+      "Transformer",
+      "numpy",
+      "pandas",
+      "matplotlib",
+    ],
+    category: "machine learning",
+    github: "",
+    webapp: "https://www.314e.com/dexit/",
+  },
+  {
+    id: 1,
+    title: "Penknife - Recruitment and Management Software",
+    date: "",
+    description:'I improved the accuracy of resume shortlisting by integrating a cross-encoder with BERT embeddings, which allowed for better matching of parsed candidate details with job descriptions, increasing shortlisting accuracy from 80% to 91%. Additionally, I implemented a temporal workflow for resume shortlisting using Background Tasks, significantly reducing API response delay. My contributions also included designing and implementing the database schema with SQLAlchemy and PostgreSQL, ensuring scalable and efficient data management, and enhancing application security by integrating Casbin middleware for role-based authorization within the API.',
+    image:penknife,
+    tags: ["Python", "FastAPI", "PostgreSQL", "Redis", "Jinja2", "Pytest", "SqlAlchemy", "RAG", "BERT", "Vecor Database", "Vespa.ai", "Alembic", "REST API"],
+    category: "machine learning",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 2,
+    title: "Jeeves - AI-powered Healthcare Training Platform",
+    date: "",
+    description:' I developed and integrated a chatbot using RAG-based retrieval and BERT embeddings, which enhanced the interactivity of training sessions. Additionally, I leveraged Vespa.ai to implement high-performance vector search, ensuring efficient data retrieval and secure handling within the platform.',
+    image: jeeves,
+    tags: ["python", "chatbot", "Generative AI", "S3", "NLP", "OpenAI", "git/github", "DSA"],
+    category: "machine learning",
+    github: "",
+    webapp: "",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Started my colllege life" },
+  { year: 2020, text: "Entered in the world of AI/ML" },
+  { year: 2022, text: "Started my professional journey" },
+];
